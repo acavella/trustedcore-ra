fbdedd463ad1f3700f985b446713f636b5623979
Tony Cavella <9434191+acavella@users.noreply.github.com>
2022-12-18 01:39:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Trusted Core: Registration Authority 
+A custom client designed to perform registration authority functions utilizing the Registration Authority Management Interface (RAMI) secure API.
+
+## Prerequisites
+
+## Installation
+
+## Usage
+
+## License & Credits
+
+### License
+
+### Author
---
+++ tcra.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# NAME: tcra.sh
+# DECRIPTION: Custom regsitration authority client used with ISC RAMI API
+# AUTHOR: Tony Cavella (tony@cavella.com)
+# SOURCE: https://github.com/acavella/trustedcore-ra
+
+## CONFIGURE DEFAULT ENVIRONMENT
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+## VARIABLES
+__dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+__bin="${__dir}/bin"
+__conf="${__dir}/conf"
+dtgf=$(date '+%Y%m%d-%H%M%S')
+ver=$(<VERSION)
+config="${__conf}""/tcra.conf"
+log="${__dir}""/log/revoke-""${dtgf}"".log"
+input=${1}
+
+startup() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info]  Trusted Core: RA v${ver} started" >> $log
+    ## CHECK AND LOAD EXTERNAL CONFIG
+    if [ ! -e $config ]
+    then
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [error] Configuration file missing" >> $log
+        exit 1
+    else
+        source $config
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Configuration file loaded sucessfully, ${config}" >> $log
+    fi
+}
+
+gen_ecdsa_csr() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Reading input file to memory, ${input}" >> $log
+    local subject=$(cat ${input})
+    local filesize=$(stat --format=%s "${input}")
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Completed reading input file, ${filesize} bytes, ${input}" >> $log
+    echo $filesize
+    local counter=0
+
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Generating private key and csr for each subject" >> $log
+    for i in $subject
+    do 
+        echo $i
+        local outputdir="${__dir}/output/${i}"
+        local csr="${outputdir}/${i}.csr"
+        local pkey="${outputdir}/${i}.key"
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Creating directory" >> $log
+        mkdir ${outputdir}
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Directory created, ${outputdir}" >> $log
+
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Generating private key for ${i}" >> $log
+        openssl ecparam -name secp384r1 -genkey -noout -out ${pkey}
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Private key generated, ${i}.key" >> $log
+
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Generating PKCS#10 CSR for ${i}" >> $log
+        openssl req -new -key "${outputdir}/${i}.key" -nodes -out ${csr} -sha384 -subj "/CN=${i}/" -config "${__conf}/ecdsa.cnf"
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] PKCS#10 CSR generated, ${i}.csr" >> $log
+
+        counter=$(( counter + 1 ))
+    done
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] Completed generating ${counter} private key and csr pairs" >> $log
+}
+
+startup
+gen_ecdsa_csr
+
+echo $input
+
+exit 0
